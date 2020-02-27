5db87a796ca18a907ebf6a0201f9f91d26b136bb
Marc Stammerjohann <marcstammerjohann@web.de>
2020-02-27 21:43:32 +0100

    feat(prisma): add lifecycle events for connection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
   constructor() {
     super();
+  }
+
+  async onModuleInit() {
+    await this.connect();
+  }
+
+  async onModuleDestroy() {
+    await this.disconnect();
   }
 }
