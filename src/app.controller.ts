32c5ed0dc5bf1d681878633aa6f2d5e753775be5
Marc Stammerjohann <marcstammerjohann@web.de>
2020-02-27 22:39:45 +0100

    feat(controller): query prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { CreateUserDto } from './create-user.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('users')
+  async users() {
+    return await this.prisma.user.findMany();
+  }
+
+  @Get('users/:id')
+  async user(@Param('id') id: string) {
+    return await this.prisma.user.findOne({ where: { id: +id } });
+  }
+
+  @Post('user')
+  async addUser(@Body() createUserDto: CreateUserDto) {
+    return await this.prisma.user.create({ data: createUserDto });
   }
 }
